7a9dfe809939181b2e5af234ef85bec05bf41f6e
Zzz <82641682+JeansChau@users.noreply.github.com>
2023-10-18 18:20:44 +0800

    Add core files in src.

---
+++ Src/Core.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zzz on 2023/10/18.
+//
+
+#include "Core.h"
---
+++ Src/Core.h
@@ -0,0 +1,11 @@
+//
+// Created by Zzz on 2023/10/18.
+//
+
+#ifndef WHITE_CORE_H
+#define WHITE_CORE_H
+namespace White::Core {
+    void Init();
+    void ShutDown();
+}
+#endif //WHITE_CORE_H
