ad960da23e6a4728606ac6f2b6e8f23277a6a1a6
Zzz <82641682+JeansChau@users.noreply.github.com>
2023-10-18 18:41:48 +0800

    Implement the Init function.

--- Src/Core.cpp
+++ Src/Core.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "Core.h"
+
+namespace White {
+    void Init(const White::InitOptions &options) {
+        //if (options.api == API::Vulkan)
+            //initVK(options);
+    }
+}
--- Src/Core.h
+++ Src/Core.h
@@ -4,8 +4,41 @@
 
 #ifndef WHITE_CORE_H
 #define WHITE_CORE_H
-namespace White::Core {
-    void Init();
+
+#include <functional>
+
+namespace White {
+    enum class API : uint8_t {
+        Noop,
+        Metal,
+        Dx12,
+        Vulkan,
+        WebGPU
+    };
+
+    enum class MessageSeverity : uint8_t {
+        Info,
+        Warning,
+        Error,
+        Fatal
+    };
+
+    using CallbackType = std::function<void(MessageSeverity, const string&)>;
+    struct InitOptions {
+        API api = API::Vulkan;
+        CallbackType callback;
+
+        struct Version{
+            uint8_t variant = 0;
+            uint8_t major = 0;
+            uint8_t minor = 0;
+            uint8_t patch = 0;
+        }
+        appVersion,
+        engineVersion;
+    };
+
+    void Init(const InitOptions& options);
     void ShutDown();
 }
 #endif //WHITE_CORE_H
